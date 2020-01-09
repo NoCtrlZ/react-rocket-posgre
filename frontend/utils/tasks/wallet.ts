26339034468d0863d6f801fc81bd931e9fadc3a5
NoCtrlZ <phantomofrotten@gmail.com>
2020-01-09 04:01:13 -0800

    skaffold k8s base but need to be fixed

--- frontend/utils/tasks/wallet.ts
+++ frontend/utils/tasks/wallet.ts
@@ -7,9 +7,9 @@ declare global {
 
 export async function getWalletAddress() {
     if (window.ethereum) {
-        window.web3 = new Web3(ethereum);
+        window.web3 = new Web3(window['ethereum']);
         try {
-            let accounts = await ethereum.enable();
+            let accounts = await window['ethereum'].enable();
             return accounts[0]
         }
         catch (err) {
